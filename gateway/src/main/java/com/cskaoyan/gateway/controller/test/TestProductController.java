1d7d687d91bd2ac09629198d1d2286b813dd1a34
Han Xiao <1043599451@qq.com>
2022-06-14 14:38:57 +0800

    TestProductDetailRequest&Response

--- gateway/src/main/java/com/cskaoyan/gateway/controller/test/TestProductController.java
+++ gateway/src/main/java/com/cskaoyan/gateway/controller/test/TestProductController.java
@@ -2,8 +2,11 @@ package com.cskaoyan.gateway.controller.test;
 
 
 import com.mall.commons.result.ResponseData;
+import com.mall.commons.result.ResponseUtil;
 import com.mall.shopping.ITestProductDetailService;
-import com.mall.shopping.dto.TestProductDetailDto;
+import com.mall.shopping.constants.ShoppingRetCode;
+import com.mall.shopping.dto.TestProductDetailRequest;
+import com.mall.shopping.dto.TestProductDetailResponse;
 import com.mall.user.annotation.Anoymous;
 import org.apache.dubbo.config.annotation.Reference;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,16 +20,29 @@ public class TestProductController {
     @Reference
     ITestProductDetailService iTestProductDetailService;
 
+//    @GetMapping("product")
+//    @Anoymous
+//    public ResponseData getProductDetail(Long productId) {
+//        TestProductDetailDto productDetail = iTestProductDetailService.getProductDetail(productId);
+//        ResponseData responseData = new ResponseData();
+//        responseData.setCode(200);
+//        responseData.setResult(productDetail);
+//        responseData.setMessage("获取商品信息成功");
+//        responseData.setSuccess(true);
+//        return responseData;
+//    }
+
     @GetMapping("product")
     @Anoymous
     public ResponseData getProductDetail(Long productId) {
-        TestProductDetailDto productDetail = iTestProductDetailService.getProductDetail(productId);
-        ResponseData responseData = new ResponseData();
-        responseData.setCode(200);
-        responseData.setResult(productDetail);
-        responseData.setMessage("获取商品信息成功");
-        responseData.setSuccess(true);
-        return responseData;
+        TestProductDetailRequest request = new TestProductDetailRequest();
+        request.setProductId(productId);
+
+        TestProductDetailResponse productDetail = iTestProductDetailService.getProductDetail(request);
+        if (ShoppingRetCode.SUCCESS.equals(productDetail.getCode())) {
+            return new ResponseUtil().setData(productDetail.getTestProductDetailDto());
+        }
+        return new ResponseUtil().setErrorMsg(productDetail.getMsg());
     }
 
 }
--- shopping-service/shopping-provider/src/main/java/com/mall/shopping/services/TestProductDetailServiceImpl.java
+++ shopping-service/shopping-provider/src/main/java/com/mall/shopping/services/TestProductDetailServiceImpl.java
@@ -8,7 +8,6 @@ import com.mall.shopping.dal.persistence.ItemMapper;
 import com.mall.shopping.dto.TestProductDetailDto;
 import com.mall.shopping.dto.TestProductDetailRequest;
 import com.mall.shopping.dto.TestProductDetailResponse;
-import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
 import org.apache.dubbo.config.annotation.Service;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -21,11 +20,34 @@ public class TestProductDetailServiceImpl implements ITestProductDetailService {
     @Autowired
     TestProductConverter testProductConverter;
 
-    @Override
+//    @Override
     public TestProductDetailDto getProductDetail(Long productId) {
-        final Item item = itemMapper.selectByPrimaryKey(productId);
-
-        TestProductDetailDto testProductDetailDto = testProductConverter.productDOToDTO(item);
+        TestProductDetailDto testProductDetailDto = null;
+        try {
+            final Item item = itemMapper.selectByPrimaryKey(productId);
+            testProductDetailDto = testProductConverter.productDOToDTO(item);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         return testProductDetailDto;
+    }
+
+    @Override
+    public TestProductDetailResponse getProductDetail(TestProductDetailRequest request) {
+
+        TestProductDetailResponse response = new TestProductDetailResponse();
+        try {
+            request.requestCheck();
+            final Item item = itemMapper.selectByPrimaryKey(request.getProductId());
+            TestProductDetailDto testProductDetailDto = testProductConverter.productDOToDTO(item);
+            response.setCode(ShoppingRetCode.SUCCESS.getCode());
+            response.setMsg(ShoppingRetCode.SUCCESS.getMessage());
+            response.setTestProductDetailDto(testProductDetailDto);
+        } catch (Exception e) {
+            e.printStackTrace();
+            response.setCode(ShoppingRetCode.DB_EXCEPTION.getCode());
+            response.setMsg(ShoppingRetCode.DB_EXCEPTION.getMessage());
+        }
+        return response;
     }
 }
